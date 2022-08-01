b4aca294fbf0bcc45973acbb9cfedf862e157929
Gadgeter-EA <0233905@up.mx>
2022-08-01 13:18:24 -0500

    Added query and toggle to Mobile Menu.

--- frontend-developer-1/main.js
+++ frontend-developer-1/main.js
@@ -1,8 +1,11 @@
-function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive");
+function toggleMenu(menu) {
+    menu.classList.toggle("inactive");
 }
 
 const navBarEmail = document.querySelector(".nav-bar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-navBarEmail.addEventListener("click", toggleDesktopMenu);
+navBarEmail.addEventListener("click", () => toggleMenu(desktopMenu));
+burgerIcon.addEventListener("click", () => toggleMenu(mobileMenu));
