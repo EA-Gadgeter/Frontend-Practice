3215e01ff4ae74c5e54be1e6128d7b961bbdce17
Gadgeter-EA <0233905@up.mx>
2022-08-10 17:31:55 -0500

    Finished cart, products now can be removed, fixed a bug with the price not adding correctly, little design improvements for mobile version

--- frontend-developer-1/main.js
+++ frontend-developer-1/main.js
@@ -96,18 +96,31 @@ function renderProducts(productList) {
 }
 
 function addToCart(buttonElement) {
+    /* There are 2 buttons to add a product to the cart, depend on which one is clicked
+    * we search for its main parent container*/
     const imageContainer = buttonElement.closest(".product-card") ?? buttonElement.closest(".product-detailed");
-    let shoppingCartImage;
+    let shoppingCartImage, price, name;
 
+    /*We search for the product info, so we can show it in the cart
+    * depending on the type of parten container, we do different things*/
     if(imageContainer.classList.contains("product-card")){
+        /* Getting the img tag from the childNodes of the imageContainer, we
+        get the right index by debugging console.log({imageContainer}) */
         shoppingCartImage = imageContainer.childNodes[0].getAttribute("src");
+        const productInfoContainer = imageContainer.childNodes[1].childNodes[0];
+        // Getting the <p> tags, and accessing its content, saving them in variables
+        price = productInfoContainer.childNodes[0].innerText;
+        name = productInfoContainer.childNodes[1].innerText;
     }
     else if(imageContainer.classList.contains("product-detailed")){
         shoppingCartImage = imageContainer.childNodes[5].getAttribute("src");
+        const productInfoContainer = productDetailed.childNodes[7];
+        price = productInfoContainer.childNodes[1].innerText;
+        name = productInfoContainer.childNodes[3].innerText;
     }
 
-    const shoppingCart = document.createElement("div");
-    shoppingCart.classList.add("shopping-cart");
+    const cardProduct = document.createElement("div");
+    cardProduct.classList.add("shopping-cart");
 
     const shoppingCartFigure = document.createElement("figure");
     const figureImage = document.createElement("img");
@@ -116,16 +129,28 @@ function addToCart(buttonElement) {
 
     const productName = document.createElement("p");
     const productPrice = document.createElement("p");
-    productName.innerText = "Bike";
-    productPrice.innerText = "$30";
+    productName.innerText = name;
+    productPrice.innerText = price;
 
     const iconClose = document.createElement("img");
     iconClose.setAttribute("src", "./icons/icon_close.png");
+    iconClose.addEventListener("click", () => removeFromCart(iconClose));
 
-    shoppingCart.append(shoppingCartFigure, productName, productPrice, iconClose);
+    cardProduct.append(shoppingCartFigure, productName, productPrice, iconClose);
 
-    myOrderContent.appendChild(shoppingCart);
+    myOrderContent.insertBefore(cardProduct, order);
 
+    /* When adding a new product to the cart, the amount of the total to pay increases, we trim the
+    * current price because for some reason has a lot of spaces, and we slice it to remove the $ sign,
+    * so we can convert it to a  with getting NaN*/
+
+    const currentPrice = shoppingCartTotal.innerText.trim().slice(1, shoppingCartTotal.innerText.length);
+    price = price.slice(1, price.length);
+
+    // We update de price
+    shoppingCartTotal.innerText = "$" + (parseInt(currentPrice) + parseInt(price));
+
+    // Updating the total of products we have in our cart.
     numberProducts.innerText = parseInt(numberProducts.innerHTML) + 1;
 }
 
@@ -148,6 +173,19 @@ function modifyProductDetailed(productImg, productPrice, productName){
     productDetailedName.innerText = productName;
 }
 
+function removeFromCart(buttonElement) {
+    const buttonContainer = buttonElement.closest(".shopping-cart");
+    let priceProduct = buttonContainer.childNodes[2];
+    priceProduct = priceProduct.innerText.slice(1, priceProduct.innerText.length);
+
+    const currentPrice = shoppingCartTotal.innerText.trim().slice(1, shoppingCartTotal.innerText.length)
+    shoppingCartTotal.innerText = "$" + (parseInt(currentPrice) - parseInt(priceProduct));
+
+    // Removing the product form the cart
+    buttonContainer.remove();
+    numberProducts.innerText = parseInt(numberProducts.innerHTML) - 1;
+}
+
 const navBarEmail = document.querySelector(".nav-bar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const burgerIcon = document.querySelector(".menu");
@@ -160,14 +198,14 @@ const productDetailedClose = document.querySelector(".product-detailed-close");
 const numberProducts = document.querySelector("#number-products");
 const addToCartButton = document.querySelector(".add-to-cart-button");
 const myOrderContent = document.querySelector(".my-order-content");
-
+const shoppingCartTotal = document.querySelector("#shopping-cart-total");
+const order = document.querySelector(".order");
 
 navBarEmail.addEventListener("click", () => toggleMenu(desktopMenu));
 burgerIcon.addEventListener("click", () => toggleMenu(mobileMenu));
 cartIcon.addEventListener("click", () => toggleMenu(shoppingCart));
 productDetailedClose.addEventListener("click", () => toggleMenu(productDetailedClose));
 addToCartButton.addEventListener("click", () => addToCart(addToCartButton));
-
 
 const productList = [];
 
